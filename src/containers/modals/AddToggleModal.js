ca751921a96df9b60089db5fd2e75b1be26cd049
Philip Wood <philip.m.wood@gmail.com>
2018-05-19 17:02:57 +0100

    Improve input and validation for toggles

--- src/containers/modals/AddToggleModal.js
+++ src/containers/modals/AddToggleModal.js
@@ -4,6 +4,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ControlLabel, FormControl, FormGroup, Glyphicon, Modal, Button } from 'react-bootstrap'
 import { hideModal } from '../../actions/index';
+import validator from 'validator';
+
+const KEY_WHITELIST = 'a-z0-9_.-';
 
 class AddToggleModal extends Component {
   constructor(props){
@@ -15,22 +18,50 @@ class AddToggleModal extends Component {
     };
   }
 
+
+
   handleOkClick = (event) => {
     if (this.isValid()) {
-        this.props.hideModal().then(() => {
-          this.props.addToggle(this.props.projectId, this.state.key, this.state.name);
-        });
-      }
-      }
+      this.props.hideModal().then(() => {
+        this.props.addToggle(this.props.projectId, this.state.key, this.state.name);
+      });
+    }
+  }
 
   handleChange = (event) => {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+
+    if (name === 'key') {
+      value = this.sanitizeKey(value);
+    }
 
     this.setState({
       [name]: value
     });
+
+    if (name === 'name') {
+      this.syncKey(value);
+    }
+  }
+
+  handleKeyFocus = (event) => {
+    this.setState({['keyFocussed']:true})
+  }
+
+  syncKey = (value) => {
+    if (this.state.key.length === 0 && this.state.keyFocussed){
+      this.setState({['keyFocussed']:false})
+    }
+
+    if (!this.state.keyFocussed){
+      this.setState({['key']: this.sanitizeKey(value)});
+    }
+  }
+
+  sanitizeKey = (value) => {
+    return validator.whitelist(value.normalize('NFKD').replace(/\s+/g, '-').toLowerCase(), KEY_WHITELIST);
   }
 
   isValid = () => {
@@ -48,6 +79,12 @@ class AddToggleModal extends Component {
     if (this.state.key.length === 0){
         return 'error';
     }
+
+    if (!validator.matches(this.state.key, `^[${KEY_WHITELIST}]*$`))
+    {
+      return 'error';
+    }
+
     return 'success';    
   } 
 
@@ -82,6 +119,7 @@ class AddToggleModal extends Component {
                 value={this.state.key}
                 placeholder="Enter text"
                 onChange={this.handleChange}
+                onFocus={this.handleKeyFocus}
               />
               <FormControl.Feedback />
             </FormGroup>            
