64ae2cfd4f91915d428c716fef984db25fe46aa9
Philip Wood <philip.m.wood@gmail.com>
2018-06-01 10:47:18 +0100

    Fix focus on add environment modal

--- src/containers/modals/AddEnvironmentModal.js
+++ src/containers/modals/AddEnvironmentModal.js
@@ -112,7 +112,6 @@ class AddEnvironment extends Component {
             <FormGroup controlId="environmentKey" validationState={this.getKeyValidationState()}>
               <ControlLabel>Environment key</ControlLabel>
               <FormControl
-                autoFocus
                 type="text"
                 name="key"
                 value={this.state.key}
