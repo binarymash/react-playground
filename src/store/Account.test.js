80fce54145348070f96a9c131911703d5abee5da
Philip Wood <philip.m.wood@gmail.com>
2019-01-01 17:42:27 +0000

    Refactor tests

--- src/store/Account.test.js
+++ src/store/Account.test.js
@@ -2,22 +2,28 @@ import { reducer } from './Account';
 import * as actionTypes from '../actions/types';
 
 it('should return the initial state', () => {
-  expect(reducer(undefined, {})).toEqual({
+  const currentState = undefined;
+  const action = {};
+  const expectedNewState = {
     projection: {
       account: {
         projects: []
       }
     },
     isLoading: false
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle INITIALISED', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.initialised
-    })
-  ).toEqual({
+  const currentState = undefined;
+
+  const action = {
+    type: actionTypes.initialised
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: []
@@ -25,56 +31,71 @@ it('should handle INITIALISED', () => {
     },
     isLoading: false,
     isInitialised: true
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle REQUEST_ACCOUNT', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.requestAccount
-    })
-  ).toEqual({
+  const currentState = undefined;
+
+  const action = {
+    type: actionTypes.requestAccount
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: []
       }
     },
     isLoading: true
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ACCOUNT', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.receiveAccount,
-      json: {
-        account: {
-          projects: [
-            {
-              id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-              name: 'My First Project'
-            },
-            {
-              id: '1fbde650-09cf-11e9-8a6f-775854a0b1e9',
-              name: 'my new environment'
-            }
-          ],
-          accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
-          audit: {
-            created: '2018-12-27T11:48:19.5302129+00:00',
-            createdBy: 'SystemUser',
-            lastModified: '2018-12-27T12:01:20.7401734+00:00',
-            lastModifiedBy: 'AnonymousUser',
-            version: 2
-          }
-        },
-        audit: {
-          generated: '2018-12-31T16:36:12.5757663+00:00',
-          streamPosition: 13
-        }
+  const currentState = {
+    projection: {
+      account: {
+        projects: []
       }
-    })
-  ).toEqual({
+    },
+    isLoading: true
+  };
+
+  const action = {
+    type: actionTypes.receiveAccount,
+    json: {
+      account: {
+        projects: [
+          {
+            id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+            name: 'My First Project'
+          },
+          {
+            id: '1fbde650-09cf-11e9-8a6f-775854a0b1e9',
+            name: 'my new environment'
+          }
+        ],
+        accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
+        audit: {
+          created: '2018-12-27T11:48:19.5302129+00:00',
+          createdBy: 'SystemUser',
+          lastModified: '2018-12-27T12:01:20.7401734+00:00',
+          lastModifiedBy: 'AnonymousUser',
+          version: 2
+        }
+      },
+      audit: {
+        generated: '2018-12-31T16:36:12.5757663+00:00',
+        streamPosition: 13
+      }
+    }
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: [
@@ -102,59 +123,72 @@ it('should handle RECEIVE_ACCOUNT', () => {
       }
     },
     isLoading: false
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ACCOUNT_ERROR', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.receiveAccountError
-    })
-  ).toEqual({
+  const currentState = {
+    projection: {
+      account: {
+        projects: []
+      }
+    },
+    isLoading: true
+  };
+
+  const action = {
+    type: actionTypes.receiveAccountError,
+    error: 'some error'
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: []
       }
     },
     isLoading: false
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle PROJECT_ADD_SUCCEEDED', () => {
-  expect(
-    reducer(
-      {
-        projection: {
-          account: {
-            projects: [
-              {
-                id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-                name: 'My First Project'
-              }
-            ],
-            accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
-            audit: {
-              created: '2018-12-27T11:48:19.5302129+00:00',
-              createdBy: 'SystemUser',
-              lastModified: '2018-12-27T12:01:20.7401734+00:00',
-              lastModifiedBy: 'AnonymousUser',
-              version: 2
-            }
-          },
-          audit: {
-            generated: '2018-12-31T16:36:12.5757663+00:00',
-            streamPosition: 13
+  const currentState = {
+    projection: {
+      account: {
+        projects: [
+          {
+            id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+            name: 'My First Project'
           }
-        },
-        isLoading: false
+        ],
+        accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
+        audit: {
+          created: '2018-12-27T11:48:19.5302129+00:00',
+          createdBy: 'SystemUser',
+          lastModified: '2018-12-27T12:01:20.7401734+00:00',
+          lastModifiedBy: 'AnonymousUser',
+          version: 2
+        }
       },
-      {
-        type: actionTypes.projectAddSucceeded,
-        id: '8960F481-E25C-442A-AA8B-67A772658D37',
-        name: 'Another project'
+      audit: {
+        generated: '2018-12-31T16:36:12.5757663+00:00',
+        streamPosition: 13
       }
-    )
-  ).toEqual({
+    },
+    isLoading: false
+  };
+
+  const action = {
+    type: actionTypes.projectAddSucceeded,
+    id: '8960F481-E25C-442A-AA8B-67A772658D37',
+    name: 'Another project'
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: [
@@ -179,44 +213,45 @@ it('should handle PROJECT_ADD_SUCCEEDED', () => {
       audit: undefined
     },
     isLoading: false
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle PROJECT_DELETE_SUCCEEDED', () => {
-  expect(
-    reducer(
-      {
-        projection: {
-          account: {
-            projects: [
-              {
-                id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-                name: 'My First Project'
-              },
-              {
-                id: '8960F481-E25C-442A-AA8B-67A772658D37',
-                name: 'Another project'
-              }
-            ],
-            accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
-            audit: {
-              created: '2018-12-27T11:48:19.5302129+00:00',
-              createdBy: 'SystemUser',
-              lastModified: undefined,
-              lastModifiedBy: undefined,
-              version: undefined
-            }
+  const currentState = {
+    projection: {
+      account: {
+        projects: [
+          {
+            id: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+            name: 'My First Project'
           },
-          audit: undefined
-        },
-        isLoading: false
+          {
+            id: '8960F481-E25C-442A-AA8B-67A772658D37',
+            name: 'Another project'
+          }
+        ],
+        accountId: 'e70fd009-22c4-44e0-ab13-2b6edaf0bbdb',
+        audit: {
+          created: '2018-12-27T11:48:19.5302129+00:00',
+          createdBy: 'SystemUser',
+          lastModified: undefined,
+          lastModifiedBy: undefined,
+          version: undefined
+        }
       },
-      {
-        type: actionTypes.projectDeleteSucceeded,
-        id: '8960F481-E25C-442A-AA8B-67A772658D37'
-      }
-    )
-  ).toEqual({
+      audit: undefined
+    },
+    isLoading: false
+  };
+
+  const action = {
+    type: actionTypes.projectDeleteSucceeded,
+    id: '8960F481-E25C-442A-AA8B-67A772658D37'
+  };
+
+  const expectedNewState = {
     projection: {
       account: {
         projects: [
@@ -237,5 +272,7 @@ it('should handle PROJECT_DELETE_SUCCEEDED', () => {
       audit: undefined
     },
     isLoading: false
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
--- src/store/Environment.test.js
+++ src/store/Environment.test.js
@@ -2,65 +2,71 @@ import { reducer } from './Environment';
 import * as actionTypes from '../actions/types';
 
 it('should return the initial state', () => {
-  expect(reducer(undefined, {})).toEqual({
+  const currentState = undefined;
+  const action = {};
+  const expectedNewState = {
     environments: {},
     environmentStates: {}
-  });
+  };
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle REQUEST_ENVIRONMENT', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.requestEnvironment,
-      projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
-      environmentKey: 'some-environment'
-    })
-  ).toEqual({
+  const currentState = undefined;
+
+  const action = {
+    type: actionTypes.requestEnvironment,
+    projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
+    environmentKey: 'some-environment'
+  };
+
+  const expectedNewState = {
     environments: {
       '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
         isLoading: true
       }
     },
     environmentStates: {}
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ENVIRONMENT', () => {
-  expect(
-    reducer(
-      {
-        environments: {
-          '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
-            isLoading: true
-          }
-        },
-        environmentStates: {}
-      },
-      {
-        type: actionTypes.receiveEnvironment,
-        projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
-        environmentKey: 'some-environment',
-        json: {
-          environment: {
-            projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-            key: 'some-environment',
-            name: 'My First Environment',
-            audit: {
-              created: '2018-12-27T11:48:19.7507748+00:00',
-              createdBy: 'SystemUser',
-              lastModified: '2018-12-27T11:48:19.7507748+00:00',
-              lastModifiedBy: 'SystemUser',
-              version: 1
-            }
-          },
-          audit: {
-            generated: '2018-12-31T18:09:15.6839632+00:00',
-            streamPosition: 6
-          }
-        }
+  const currentState = {
+    environments: {
+      '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
+        isLoading: true
       }
-    )
-  ).toEqual({
+    },
+    environmentStates: {}
+  };
+
+  const action = {
+    type: actionTypes.receiveEnvironment,
+    projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
+    environmentKey: 'some-environment',
+    json: {
+      environment: {
+        projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+        key: 'some-environment',
+        name: 'My First Environment',
+        audit: {
+          created: '2018-12-27T11:48:19.7507748+00:00',
+          createdBy: 'SystemUser',
+          lastModified: '2018-12-27T11:48:19.7507748+00:00',
+          lastModifiedBy: 'SystemUser',
+          version: 1
+        }
+      },
+      audit: {
+        generated: '2018-12-31T18:09:15.6839632+00:00',
+        streamPosition: 6
+      }
+    }
+  };
+
+  const expectedNewState = {
     environments: {
       '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
         environment: {
@@ -83,87 +89,93 @@ it('should handle RECEIVE_ENVIRONMENT', () => {
       }
     },
     environmentStates: {}
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ENVIRONMENT_ERROR', () => {
-  expect(
-    reducer(
-      {
-        environments: {
-          '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
-            isLoading: true
-          }
-        },
-        environmentStates: {}
-      },
-      {
-        type: actionTypes.receiveEnvironmentError,
-        projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
-        environmentKey: 'some-environment',
-        error: 'some error'
+  const currentState = {
+    environments: {
+      '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
+        isLoading: true
       }
-    )
-  ).toEqual({
+    },
+    environmentStates: {}
+  };
+
+  const action = {
+    type: actionTypes.receiveEnvironmentError,
+    projectId: '2DBE229D-5318-4AD5-A62E-3F3D260C850F',
+    environmentKey: 'some-environment',
+    error: 'some error'
+  };
+
+  const expectedNewState = {
     environments: {
       '2DBE229D-5318-4AD5-A62E-3F3D260C850F/some-environment': {
         isLoading: false
       }
     },
     environmentStates: {}
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle REQUEST_ENVIRONMENTSTATE', () => {
-  expect(
-    reducer(undefined, {
-      type: actionTypes.requestEnvironmentState,
-      projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-      environmentKey: 'my-first-environment'
-    })
-  ).toEqual({
+  const currentState = undefined;
+
+  const action = {
+    type: actionTypes.requestEnvironmentState,
+    projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+    environmentKey: 'my-first-environment'
+  };
+
+  const expectedNewState = {
     environments: {},
     environmentStates: {
       '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
         isLoading: true
       }
     }
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ENVIRONMENTSTATE', () => {
-  expect(
-    reducer(
-      {
-        environments: {},
-        environmentStates: {
-          '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
-            isLoading: true
-          }
-        }
-      },
-      {
-        type: actionTypes.receiveEnvironmentState,
-        projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-        environmentKey: 'my-first-environment',
-        json: {
-          environmentState: {
-            toggleStates: [
-              {
-                key: 'my-first-toggle',
-                value: 'False',
-                version: 12
-              }
-            ]
-          },
-          audit: {
-            generated: '2018-12-31T18:09:15.8617841+00:00',
-            streamPosition: 19
-          }
-        }
+  const currentState = {
+    environments: {},
+    environmentStates: {
+      '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
+        isLoading: true
       }
-    )
-  ).toEqual({
+    }
+  };
+
+  const actions = {
+    type: actionTypes.receiveEnvironmentState,
+    projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+    environmentKey: 'my-first-environment',
+    json: {
+      environmentState: {
+        toggleStates: [
+          {
+            key: 'my-first-toggle',
+            value: 'False',
+            version: 12
+          }
+        ]
+      },
+      audit: {
+        generated: '2018-12-31T18:09:15.8617841+00:00',
+        streamPosition: 19
+      }
+    }
+  };
+
+  const expectedNewState = {
     environments: {},
     environmentStates: {
       '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
@@ -183,70 +195,72 @@ it('should handle RECEIVE_ENVIRONMENTSTATE', () => {
         isLoading: false
       }
     }
-  });
+  };
+
+  expect(reducer(currentState, actions)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_ENVIRONMENTSTATE_ERROR', () => {
-  expect(
-    reducer(
-      {
-        environments: {},
-        environmentStates: {
-          '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
-            isLoading: true
-          }
-        }
-      },
-      {
-        type: actionTypes.receiveEnvironmentStateError,
-        projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-        environmentKey: 'my-first-environment',
-        error: 'some error'
+  const currentState = {
+    environments: {},
+    environmentStates: {
+      '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
+        isLoading: true
       }
-    )
-  ).toEqual({
+    }
+  };
+
+  const action = {
+    type: actionTypes.receiveEnvironmentStateError,
+    projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+    environmentKey: 'my-first-environment',
+    error: 'some error'
+  };
+
+  const expectedNewState = {
     environments: {},
     environmentStates: {
       '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
         isLoading: false
       }
     }
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle TOGGLESTATE_UPDATE_SUCCEEDED', () => {
-  expect(
-    reducer(
-      {
-        environments: {},
-        environmentStates: {
-          '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
-            environmentState: {
-              toggleStates: [
-                {
-                  key: 'my-first-toggle',
-                  value: 'False',
-                  version: 12
-                }
-              ]
-            },
-            audit: {
-              generated: '2018-12-31T18:09:15.8617841+00:00',
-              streamPosition: 19
-            },
-            isLoading: false
-          }
-        }
-      },
-      {
-        type: actionTypes.toggleStateUpdateSucceeded,
-        projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
-        environmentKey: 'my-first-environment',
-        toggleKey: 'my-first-toggle',
-        value: 'True'
+  const currentState = {
+    environments: {},
+    environmentStates: {
+      '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
+        environmentState: {
+          toggleStates: [
+            {
+              key: 'my-first-toggle',
+              value: 'False',
+              version: 12
+            }
+          ]
+        },
+        audit: {
+          generated: '2018-12-31T18:09:15.8617841+00:00',
+          streamPosition: 19
+        },
+        isLoading: false
       }
-    )
-  ).toEqual({
+    }
+  };
+
+  const action = {
+    type: actionTypes.toggleStateUpdateSucceeded,
+    projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+    environmentKey: 'my-first-environment',
+    toggleKey: 'my-first-toggle',
+    value: 'True'
+  };
+
+  const expectedNewState = {
     environments: {},
     environmentStates: {
       '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-environment': {
@@ -263,5 +277,7 @@ it('should handle TOGGLESTATE_UPDATE_SUCCEEDED', () => {
         isLoading: false
       }
     }
-  });
+  };
+
+  expect(reducer(currentState, action)).toEqual(expectedNewState);
 });
