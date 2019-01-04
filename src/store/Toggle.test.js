062e1aac93aa9a7a942a777ed13d4e6977f1bcd0
Philip Wood <philip.m.wood@gmail.com>
2019-01-04 08:24:07 +0000

    Add happy path unit tests for toggle store reads

--- src/store/Toggle.test.js
+++ src/store/Toggle.test.js
@@ -1,5 +1,73 @@
-import { reducer } from './Toggle';
+import * as toggle from './Toggle';
 import * as actionTypes from '../actions/types';
+
+const nominalState = () => {
+  return {
+    toggle: {
+      toggles: {
+        '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-toggle': {
+          toggle: {
+            projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+            key: 'my-first-toggle',
+            name: 'My First Toggle',
+            audit: {
+              created: '2018-12-27T11:48:19.8836223+00:00',
+              createdBy: 'SystemUser',
+              lastModified: '2018-12-27T11:48:19.8836223+00:00',
+              lastModifiedBy: 'SystemUser',
+              version: 3
+            }
+          },
+          audit: {
+            generated: '2018-12-31T18:09:15.782011+00:00',
+            streamPosition: 8
+          },
+          isLoading: false
+        }
+      }
+    }
+  };
+};
+
+it('should return isLoading', () => {
+  const currentState = nominalState();
+  const projectId = '8f73d020-96c4-407e-8602-74fd4e2ed08b';
+  const toggleKey = 'my-first-toggle';
+
+  expect(toggle.getIsToggleLoading(currentState, projectId, toggleKey)).toEqual(
+    false
+  );
+
+  currentState.toggle.toggles[
+    '8f73d020-96c4-407e-8602-74fd4e2ed08b/my-first-toggle'
+  ].isLoading = true;
+
+  expect(toggle.getIsToggleLoading(currentState, projectId, toggleKey)).toEqual(
+    true
+  );
+});
+
+it('should return toggle', () => {
+  const currentState = nominalState();
+  const projectId = '8f73d020-96c4-407e-8602-74fd4e2ed08b';
+  const toggleKey = 'my-first-toggle';
+  const expectedResult = {
+    key: 'my-first-toggle',
+    name: 'My First Toggle',
+    projectId: '8f73d020-96c4-407e-8602-74fd4e2ed08b',
+    audit: {
+      created: '2018-12-27T11:48:19.8836223+00:00',
+      createdBy: 'SystemUser',
+      lastModified: '2018-12-27T11:48:19.8836223+00:00',
+      lastModifiedBy: 'SystemUser',
+      version: 3
+    }
+  };
+
+  expect(toggle.getToggle(currentState, projectId, toggleKey)).toEqual(
+    expectedResult
+  );
+});
 
 it('should return the initial state', () => {
   const currentState = undefined;
@@ -8,7 +76,7 @@ it('should return the initial state', () => {
     toggles: {}
   };
 
-  expect(reducer(currentState, action)).toEqual(expectedNewState);
+  expect(toggle.reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle REQUEST_TOGGLE', () => {
@@ -28,7 +96,7 @@ it('should handle REQUEST_TOGGLE', () => {
     }
   };
 
-  expect(reducer(currentState, action)).toEqual(expectedNewState);
+  expect(toggle.reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_TOGGLE', () => {
@@ -88,7 +156,7 @@ it('should handle RECEIVE_TOGGLE', () => {
     }
   };
 
-  expect(reducer(currentState, action)).toEqual(expectedNewState);
+  expect(toggle.reducer(currentState, action)).toEqual(expectedNewState);
 });
 
 it('should handle RECEIVE_TOGGLE_ERROR', () => {
@@ -115,5 +183,5 @@ it('should handle RECEIVE_TOGGLE_ERROR', () => {
     }
   };
 
-  expect(reducer(currentState, action)).toEqual(expectedNewState);
+  expect(toggle.reducer(currentState, action)).toEqual(expectedNewState);
 });
