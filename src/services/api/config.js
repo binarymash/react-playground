3169cf7569ac6f39ce3c4780b39e4e2e6f246f3e
Philip Wood <philip.m.wood@gmail.com>
2020-08-13 16:19:24 +0100

    Add authorization header to api requests

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,3 +1,5 @@
+import { Auth } from 'aws-amplify';
+
 export const QueriesApi = 'Evelyn Queries API';
 
 export const CommandsApi = 'Evelyn Commands API';
@@ -7,10 +9,26 @@ export default {
     {
       name: QueriesApi,
       endpoint: process.env.REACT_APP_API_QUERY_BASE_URL,
+      custom_header: async () => {
+        return {
+          Authorization: `Bearer ${(await Auth.currentSession())
+            .getAccessToken()
+            .getJwtToken()}`,
+        };
+        // return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+      },
     },
     {
       name: CommandsApi,
       endpoint: process.env.REACT_APP_API_COMMAND_BASE_URL,
+      custom_header: async () => {
+        return {
+          Authorization: `Bearer ${(await Auth.currentSession())
+            .getAccessToken()
+            .getJwtToken()}`,
+        };
+        // return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` }
+      },
     },
   ],
 };
