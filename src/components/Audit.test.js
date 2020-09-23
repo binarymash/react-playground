4a467646ef2f4808df2e78309613339e96933241
Philip Wood <philip.m.wood@gmail.com>
2020-09-23 13:47:09 +0100

    Fix test warning

--- src/components/Audit.test.js
+++ src/components/Audit.test.js
@@ -11,9 +11,9 @@ it('renders nominal value without crashing', () => {
     audit: {
       created: '2018-12-27T11:48:19.5302129+00:00',
       createdBy: 'Someone',
-      lastModified: '2018-12-27T12:01:20.7401734+00:003',
-      lastModifiedBy: 'Someone else'
-    }
+      lastModified: '2018-12-27T12:01:20.7401734+00:00',
+      lastModifiedBy: 'Someone else',
+    },
   };
 
   shallow(<Audit {...props} />);
